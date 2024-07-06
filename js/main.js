dd868533b6ceb88b19913163b5a901bceee5dbd4
Saulo-Ferro-Maciel <kogasaal@gmail.com>
2024-07-06 14:28:30 -0300

    Changes to be committed: 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ init();
 animationScroll('logo', 'animate__backInLeft', [0], 100);
 animationScroll('slider-container', 'animate__backInRight', [0, 0.5], 100);
 animationScroll('content', 'animate__backInUp', [0], 10);
-animationScroll('photo', 'animate__backInDown', [0], 10);
+animationScroll('photo', 'animate__backInDown', [0.5], 10);
 animationScroll('table', 'animate__bounceInLeft', [1], 1060);
 animationScroll('table2', 'animate__flash', [1], 1060);
 animationScroll('set','animate__bounceInUp',[0, 0.5], 30);
