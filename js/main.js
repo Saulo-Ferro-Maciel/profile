5a3b40d998ffb4b346b2f290eb58c8d58894262a
Saulo-Ferro-Maciel <kogasaal@gmail.com>
2024-10-05 18:47:45 -0300

    Changes to be committed: 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -7,26 +7,12 @@ async function init() {
     typeWrite(outroElemento);
 
     // Animações da Página
-    animationScroll('logo', 'animate__backInLeft', [0], 30);
-    animationScroll('slider-container', 'animate__backInRight', [0, 0.5], 30);
-    animationScroll('photo', 'animate__backInDown', [0.5], 10);
-}
-
-async function init() {
-    await atualizarSaudacao();
-    const phrase = document.querySelector('.saudacao');
-    typeWrite(phrase);
-
-    const outroElemento = document.querySelector('.headline');
-    typeWrite(outroElemento);
-
-    // Animações da Página
     animationScroll('slider-container', 'animate__backInRight', [0, 0.5], 30);
     animationScroll('logo', 'animate__backInLeft', [0], 30);
 
     // Verifica se o dispositivo é um mobile (largura da tela menor que 768px)
     if (window.innerWidth <= 768) {
-        animationScroll('photo', 'animate__backInDown', [0.5], 10);
+        animationScroll('photo', 'animate__backInDown', [0.4], 10);
     }
 }
 
@@ -38,7 +24,7 @@ init();
 animationScroll('logo', 'animate__backInLeft', [0], 100, 500);
 animationScroll('slider-container', 'animate__backInRight', [0, 0.5], 100, 500);
 animationScroll('content', 'animate__backInUp', [0], 10, 2000);
-animationScroll('photo', 'animate__backInDown', [0.5], 10, 2000);
+animationScroll('photo', 'animate__backInDown', [0,0.4], 10, 2000);
 animationScroll('table', 'animate__bounceInLeft', [1], 1060, 500);
 animationScroll('table2', 'animate__flash', [1], 1060, 500);
 animationScroll('set','animate__bounceInUp',[0, 0.5], 30, 500);
